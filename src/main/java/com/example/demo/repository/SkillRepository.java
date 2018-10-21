6600c5c83d46081bd7f6a88b7f8db4bc4bd1cd08
Neil Orzechowski <nvo2101@columbia.edu>
2018-10-21 18:48:40 -0500

    init

---
+++ src/main/java/com/example/demo/repository/SkillRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Skill;
+
+/**
+ * Created by ashish on 13/5/17.
+ */
+@Repository
+public interface SkillRepository extends JpaRepository<Skill, Integer>{
+}
