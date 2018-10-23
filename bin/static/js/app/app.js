7caeef935296430581c07afad0a9662a342f5b89
Neil Orzechowski <nvo2101@columbia.edu>
2018-10-22 21:25:54 -0500

    more rename fallout

--- bin/static/js/app/app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var devopsDemoApp = angular.module('devopsDemo', [ 'ui.bootstrap', 'devopsDemo.controllers', 'devopsDemo.services','chart.js' ,'ngTable']);
-devopsDemoApp.constant("CONSTANTS", {
-	getEmployeeByIdUrl : "/employee/getEmployee/",
-	getAllEmployees : "/employee/getAllEmployees",
-	saveEmployee : "/employee/saveEmployee",
-	getCircleChartData : "/data/getCircleChartData"
-});
